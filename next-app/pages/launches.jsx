a5a80c9578275f47580148dfa5a2deeee9705067
mooooburg-dev <jeongmupark@gmail.com>
2022-02-13 18:58:51 +0900

    Next.js 기본

---
+++ next-app/pages/launches.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+export default function Launches({ data }) {
+  // const [data, setData] = useState(null);
+
+  // useEffect(() => {
+  //   const fetchLaunches = async () => {
+  //     const res = await fetch('https://api.spacexdata.com/v3/launches');
+  //     const data = await res.json();
+
+  //     setData(data);
+  //   };
+
+  //   fetchLaunches();
+  // }, []);
+
+  if (data === null) {
+    return null;
+  }
+  return (
+    <div>
+      <ol>
+        {data.map((launch, index) => (
+          <li key={index}>{launch.mission_name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://api.spacexdata.com/v3/launches');
+  const data = await res.json();
+
+  console.log('getStaticProps');
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('https://api.spacexdata.com/v3/launches');
+  const data = await res.json();
+
+  console.log('getServerSideProps');
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+}
