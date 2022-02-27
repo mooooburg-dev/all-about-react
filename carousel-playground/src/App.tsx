794c6f4ec3f548c21544ce0ac0ad49309f447524
mooooburg-dev <jeongmupark@gmail.com>
2022-02-27 13:02:18 +0900

    ✨ Carousel

--- carousel-playground/src/App.tsx
+++ carousel-playground/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Carousel from './components/Carousel';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
